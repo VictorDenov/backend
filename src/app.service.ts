46388c26892b1da03988819105aed44fff262017
VictorDenov <victor.guaracaa@gmail.com>
2023-08-04 14:53:26 -0500

    hola

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'esta cambiado';
+    return ' estop esta muy raro';
   }
 }
